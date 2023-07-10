13dfbc053741c6719fe43a6212c585d9cb7b358f
masud-rana44 <mdmasudrana4488@gmail.com>
2023-07-10 15:00:14 +0600

    implement counter from global context

---
+++ src/components/C.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { counterContext } from "../App";
+
+function C() {
+  const { count, countDispatch } = useContext(counterContext);
+
+  return (
+    <div>
+      <div>From C - count: {count}</div>
+      <button type="button" onClick={() => countDispatch("increment")}>
+        Increment
+      </button>
+      <button type="button" onClick={() => countDispatch("decrement")}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default C;
